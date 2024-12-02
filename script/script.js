ebc19e38093c5f67783bc6c1916ce78cea206524
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2024-12-02 10:57:06 +0700

    add wording and fix

--- script/script.js
+++ script/script.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+  const burger = document.querySelector('.burger')
+  const middle = document.querySelector('.middle')
+  const inner = document.querySelector('.inner')
+
+  burger.addEventListener('click', () => {
+      middle.style.display = 'block'
+      inner.style.right = '0'
+  })
+
+  middle.addEventListener('click', () => {
+      middle.style.display = 'none'
+      inner.style.right = '-100%'
+  })
+
   var items = document.querySelectorAll(".timeline li");
   
   function isElementInViewport(el) {
